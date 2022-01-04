a6bdd572ccaa5e0dbeff43e40fe78cd2dc08b569
Leonardo Goandete <leonardo.goandete@hotmail.com>
2022-01-03 23:40:34 -0300

    Criado inserts placamaeCPU e PlacaMaeMemoria.

--- select.sql
+++ select.sql
@@ -6,3 +6,14 @@ SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.NOME AS NOME_DA_PLACA_M
     INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
     INNER JOIN Memoria ME ON C.ID_Memoria = ME.ID_Memoria
     INNER JOIN PlacaVideo PV ON C.ID_PlacaVideo = PV.ID_PlacaVideo;
+
+
+-- soma total da montagem --
+SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.Modelo AS NOME_DA_PLACA_MÃE, P.Modelo AS MODELO_CPU, ME.nome AS NOME_MEMORIA, PV.nome AS NOME_DA_PLACA_DE_VÍDEO,
+    (M.Valor+P.Valor+ME.Valor+PV.Valor) AS VALOR_TOTAL_DA_MONTAGEM
+    FROM PC C
+    INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+    INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+    INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+    INNER JOIN Memoria ME ON C.ID_Memoria = ME.ID_Memoria
+    INNER JOIN PlacaVideo PV ON C.ID_PlacaVideo = PV.ID_PlacaVideo;
