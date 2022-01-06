36a265a0cffc8967b0c7252a646b5e3aae731215
Leonardo Goandete <leonardo.goandete@hotmail.com>
2022-01-06 01:05:25 -0300

    Criação de 2 selects e ajuste constraint alter table PC_PlacaVideo

--- select.sql
+++ select.sql
@@ -56,3 +56,34 @@ FROM PLACAMAEMEMORIA pm
 INNER JOIN placamae p on pm.ID_PLACAMAE = p.ID_PLACAMAE
 INNER JOIN MEMORIA m on pm.ID_MEMORIA = m.ID_MEMORIA
 ORDER BY p.NOME;
+
+-- exibe a soma total de Montagens incluindo todas as peças e exibe o montante total das montagens --
+SELECT sum(M.Valor+P.Valor+ME.Valor+pv.valor) as VALOR_TOTAL
+FROM PC C
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = PM.id_montagem
+INNER JOIN Memoria ME ON PM.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
+where m.ARQUITETURA = 'Intel'
+;
+
+-- exibe a soma total de Montagens incluindo todas as peças sem GPU e exibe o montante total das montagens --
+SELECT sum(M.Valor+P.Valor+ME.Valor) as VALOR_TOTAL
+FROM PC C
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = PM.id_montagem
+INNER JOIN Memoria ME ON PM.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+WHERE PPV.ID_PlacaVideo is null AND m.ARQUITETURA = 'Intel'
+;
+
+-- Visualizar quais placa maes se relacionam com mais de uma CPU e Memoria
+select p2.nome, count(p.ID_CPU) AS NRO_CPUs_RELACIONADAS, count(pm.ID_MEMORIA) AS NRO_MEM_RELACIONADAS
+from PC P
+inner join PLACAMAE P2 on P.ID_PLACAMAE = P2.ID_PLACAMAE
+inner join PC_MEMORIA PM on P.ID_MONTAGEM = PM.ID_MONTAGEM
+GROUP BY p2.nome HAVING COUNT(p.ID_cpu) > 1 AND COUNT(pm.id_memoria) > 1
+;
