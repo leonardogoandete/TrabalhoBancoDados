abb1a8ca9921762bc23d85a4495b9a4508287103
Leonardo Goandete <leonardo.goandete@hotmail.com>
2022-01-05 22:31:25 -0300

    Ajustes no select

--- select.sql
+++ select.sql
@@ -1,51 +1,58 @@
 -- Montagem do PC, buscando nome dos itens da colunas usando o ID.
 
-    SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, P.Modelo AS NOME_CPU, ME.nome AS NOME_MEMORIA, PV.nome AS PLACA_VIDEO
-        FROM PC C
-        INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
-        INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
-        INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
-        INNER JOIN PC_Memoria PM ON C.ID_Montagem = pm.id_montagem
-        INNER JOIN Memoria ME ON pm.ID_MEMORIA = ME.ID_MEMORIA
-        INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
-        INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
-        ORDER BY C.ID_Montagem asc;
+SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, P.Modelo AS NOME_CPU, ME.nome AS NOME_MEMORIA, PV.nome AS PLACA_VIDEO
+FROM PC C
+INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = pm.id_montagem
+INNER JOIN Memoria ME ON pm.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
+ORDER BY C.ID_Montagem asc;
 
 --- sem gpu
-        SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, P.Modelo AS NOME_CPU, ME.nome AS NOME_MEMORIA
-            FROM PC C
-            INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
-            INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
-            INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
-            INNER JOIN PC_Memoria PM ON C.ID_Montagem = pm.id_montagem
-            INNER JOIN Memoria ME ON pm.ID_MEMORIA = ME.ID_MEMORIA
-            INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
-            WHERE PPV.ID_PlacaVideo is null
-            ORDER BY C.ID_Montagem asc;
+SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, P.Modelo AS NOME_CPU, ME.nome AS NOME_MEMORIA
+FROM PC C
+INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = pm.id_montagem
+INNER JOIN Memoria ME ON pm.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+WHERE PPV.ID_PlacaVideo is null
+ORDER BY C.ID_Montagem asc;
 
 -- soma total da montagem --
 
 SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO ,(M.Valor+P.Valor+ME.Valor+PV.Valor) as VALOR_TOTAL
-    FROM PC C
-    INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
-    INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
-    INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
-    INNER JOIN PC_Memoria PM ON C.ID_Montagem = PM.id_montagem
-    INNER JOIN Memoria ME ON PM.ID_MEMORIA = ME.ID_MEMORIA
-    INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
-    INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
-    ORDER BY C.ID_Montagem asc;
+FROM PC C
+INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = PM.id_montagem
+INNER JOIN Memoria ME ON PM.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
+ORDER BY C.ID_Montagem asc;
 
 -- exibe montagens com valores acima de 700 --
 SELECT U.ID_Conta AS ID_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, M.Valor AS VALOR_PLACA_MAE
-    FROM PC C
-    INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
-    INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
-    GROUP BY U.ID_Conta, M.NOME, M.Valor HAVING (M.Valor) > 700
-    ORDER BY U.ID_Conta;
+FROM PC C
+INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+GROUP BY U.ID_Conta, M.NOME, M.Valor HAVING (M.Valor) > 700
+ORDER BY U.ID_Conta;
 
 -- exibe total de compatibilidades de memoria para cada tipo de placa mae --
-select p.NOME, COUNT(pm.ID_MEMORIA) AS NRO_MEMORIAS_COMPATIVEIS
-from PLACAMAEMEMORIA pm
-inner join placamae p on pm.ID_PLACAMAE = p.ID_PLACAMAE
-GROUP BY p.NOME HAVING COUNT (pm.ID_MEMORIA) >= 5;
+SELECT p.NOME, COUNT(pm.ID_MEMORIA) AS NRO_MEMORIAS_COMPATIVEIS
+FROM PLACAMAEMEMORIA pm
+INNER JOIN placamae p on pm.ID_PLACAMAE = p.ID_PLACAMAE
+GROUP BY p.NOME HAVING COUNT (pm.ID_MEMORIA) >= 7;
+
+-- exibe modelos de placas agrupadas por modelo e as memorias compativeis --
+SELECT p.NOME AS MODELO_PLACAMAE, m.NOME AS MODELO_MEMORIA,m.CAPACIDADE AS CAPACIDADES, m.ARQUITETURA AS ARQUITETURA
+FROM PLACAMAEMEMORIA pm
+INNER JOIN placamae p on pm.ID_PLACAMAE = p.ID_PLACAMAE
+INNER JOIN MEMORIA m on pm.ID_MEMORIA = m.ID_MEMORIA
+ORDER BY p.NOME;
