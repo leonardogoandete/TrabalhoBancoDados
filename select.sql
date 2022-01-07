ec295ee0af368c2239aae0640e14ad99bcd0a019
Leonardo Goandete <leonardo.goandete@hotmail.com>
2022-01-06 21:54:17 -0300

    Ajuste de select e atributos.

--- select.sql
+++ select.sql
@@ -36,6 +36,18 @@ INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
 INNER JOIN PlacaVideo PV ON PPV.ID_PLACAVIDEO = PV.ID_PLACAVIDEO
 ORDER BY C.ID_Montagem asc;
 
+-- soma total da montagem gpu--
+SELECT C.ID_Montagem AS ID_PC, U.Nome AS NOME_USUARIO ,(M.Valor+P.Valor+ME.Valor) as VALOR_TOTAL
+FROM PC C
+INNER JOIN Usuario U ON C.ID_Conta = U.ID_Conta
+INNER JOIN PlacaMae M ON C.ID_PlacaMae = M.ID_PlacaMae
+INNER JOIN CPU P ON C.ID_CPU = P.ID_CPU
+INNER JOIN PC_Memoria PM ON C.ID_Montagem = PM.id_montagem
+INNER JOIN Memoria ME ON PM.ID_MEMORIA = ME.ID_MEMORIA
+INNER JOIN PC_PlacaVideo PPV ON C.ID_MONTAGEM = PPV.ID_MONTAGEM
+WHERE PPV.ID_PlacaVideo is null
+ORDER BY C.ID_Montagem asc;
+
 -- exibe montagens com valores acima de 700 --
 SELECT U.ID_Conta AS ID_USUARIO, M.NOME AS NOME_DA_PLACA_MAE, M.Valor AS VALOR_PLACA_MAE
 FROM PC C
